44b8cd18f7fc2ef76509201a5b20529e75839861
Taylor <tandras92@outlook.com>
2021-05-24 18:27:46 -0500

    updated requirements.txt and airflow dummy op

--- airflow/dags/dummy_dag.py
+++ airflow/dags/dummy_dag.py
@@ -1,5 +1,5 @@
 from airflow import DAG
-from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.dummy import DummyOperator
 from datetime import datetime
 
 with DAG('example_dag', start_date=datetime(2021, 5, 22)) as dag:
